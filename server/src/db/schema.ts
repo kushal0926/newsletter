4770dfb9855989aaf4da1ef634001f2899af9e5e
kushal tamang <kushaltamang0926@gmail.com>
2026-02-26 23:47:07 +0545

    adding drizzle with neon and postgresql for databse

---
+++ server/src/db/schema.ts
@@ -0,0 +1,29 @@
+import { boolean, integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
+
+export const subscriberTable = pgTable("subscribers", {
+    id: integer()
+        .primaryKey()
+        .generatedAlwaysAsIdentity(),
+
+    email: varchar({ length: 255 })
+        .notNull()
+        .unique(),
+
+    token: varchar("token", { length: 64 }),
+
+    confirmed: boolean("confirmed")
+        .notNull()
+        .default(false),
+
+    active: boolean("active")
+        .notNull()
+        .default(false),
+
+    createdAt: timestamp("created_at", { withTimezone: true })
+        .defaultNow()
+        .notNull(),
+
+    updatedAt: timestamp("updated_at", { withTimezone: true })
+        .defaultNow()
+        .notNull()
+});
